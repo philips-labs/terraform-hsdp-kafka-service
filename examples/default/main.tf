6fff37516585d26e3af82f0214674df46e392d70
Raj Shivaram <86063900+rajshivaram@users.noreply.github.com>
2022-04-12 13:30:58 +0530

    Updated the source

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -1,5 +1,6 @@
-module "kafka" {
-  source = "./modules/kafka"
-
+module "kafka-service" {
+  source  = "philips-labs/kafka-service/hsdp"
+  version = ">=0.0.1"
   cf_space_id = var.cf_space_id
 }
+  
